9d55a4b9375970dce75f777b0843db5d3d9d9438
TCiras <taciras@gmail.com>
2017-04-14 10:36:21 -0400

    Defines corners

--- HitBoxes.rkt
+++ HitBoxes.rkt
@@ -37,17 +37,13 @@ carHitBox = Calculates the hitbox for a car
         (w2 (/ car-width 2))  #|half the car's width|#
         (L2 (/ car-length 2)) #|half the car's length|#
         (ac (cos (- t pi)))   #|adjacent side length of triangle to edge|#
-        (as (sin (- t pi)))   #|opposite side length of triangel to edge|#
-        (ap (+ x (* L2 ac)))  #|x-cord of right most length middle|#
-        (am (- x (* L2 ac)))  #|x-cord of left most length middle|#
-        (bp (+ y (* L2 as)))  #|y-cord of right most length middle|#
-        (bm (- y (* L2 as)))) #|y-cord of left most length middle|#
-    (list (list (+ am (- L2) (* w2 ac))
-                (- bm L2 (* w2 as)))  #|(a,b)|#
-          (list (- ap (- L2) (* w2 ac))
-                (- bp L2 (* w2 as)))  #|(c,d)|#
-          (list (- ap (- L2) (* w2 ac))
-                (+ bp L2 (* w2 as)))  #|(e,f)|#
-          (list (+ am (- L2) (* w2 ac))
-                (+ bm L2 (* w2 as)))) #|(g,h)|#
+        (as (sin (- t pi))))   #|opposite side length of triangel to edge|#
+    (list (list (+ x (* w2 ac) (- (* L2 as)))
+                (+ y (* w2 as) (+ (* L2 ac))))  #|(a,b)|#
+          (list (- x (* w2 ac) (- (* L2 as)))
+                (+ y (* w2 as) (+ (* L2 ac))))  #|(c,d)|#
+          (list (- x (* w2 ac) (- (* L2 as)))
+                (- y (* w2 as) (+ (* L2 ac))))  #|(e,f)|#
+          (list (+ x (* w2 ac) (- (* L2 as)))
+                (- y (* w2 as) (+ (* L2 ac))))) #|(g,h)|#
     ))
